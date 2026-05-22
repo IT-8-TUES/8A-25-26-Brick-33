39f4bd11d438d49abb4bba677e68a86563219df5
Kris-ni <ktodorov2011@gmail.com>
2026-05-22 13:11:34 +0300

    vote finsishing touches

--- Kristiqn/vote.js
+++ Kristiqn/vote.js
@@ -11,40 +11,32 @@ closeBtn.addEventListener('click', () => {
   dialog.close();
 });
 
-
 const allVoteButtons = document.querySelectorAll('.vote-btn');
 
 allVoteButtons.forEach((voteBtn, index) => {
-    const storageKey = `song_vote_${index}`;
+    const storageKey = `song_vote_state_${index}`;
     let hasVoted = localStorage.getItem(storageKey) === 'true';
-    let currentVotes = hasVoted ? 1 : 0;
     
-    const voteCounter = voteBtn.nextElementSibling;
+    // Set initial layout state from memory
     if (hasVoted) {
         voteBtn.innerText = 'Voted';
         voteBtn.className = 'vote-btn voted';
-        voteCounter.innerText = '1';
     } else {
         voteBtn.innerText = 'Vote';
         voteBtn.className = 'vote-btn not-voted';
-        voteCounter.innerText = '0';
     }
 
-  
     voteBtn.addEventListener('click', () => {
         if (!hasVoted) {
             voteBtn.innerText = 'Voted';
             voteBtn.className = 'vote-btn voted';
-            currentVotes = 1; 
             hasVoted = true;
             localStorage.setItem(storageKey, 'true'); 
         } else {
             voteBtn.innerText = 'Vote';
             voteBtn.className = 'vote-btn not-voted';
-            currentVotes = 0; 
             hasVoted = false;
             localStorage.setItem(storageKey, 'false'); 
         }
-        voteCounter.innerText = currentVotes;
     });
 });
